6dc1ce87fc272b9ebc29ff0fa750e05e4cced59f
Marcel Caspar <marcel.caspar@sittig.de>
2018-05-16 14:48:24 +0200

    app init

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'ios-app',
+		wp_ws_url : 'https://lktf.turnjugend-frankfurt.de/wp-appkit-api/ios-app',
+		wp_url : 'https://lktf.turnjugend-frankfurt.de',
+		theme : 'q-ios-lktf',
+		version : '0.1',
+		app_type : 'phonegap-build',
+		app_title : '',
+		app_platform : 'ios',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'v=c7Wxmb{9Lkb?{m1d^%i~wQqD*5>PlVNH|kJ#^MB:X1=Y.*h8#juFaQV>+w(VpR',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
